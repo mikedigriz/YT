6c2b9db4d72df0a5c97c194124a2b500a0157477
Mihail <mik.gryzunov@gmail.com>
2023-12-01 13:56:23 +0300

    test pass

--- app/etc/Scripts/2hourcleanup.sh
+++ app/etc/Scripts/2hourcleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# for adding to cron
-# but i have no time for test it in docker
+# add cronjob for autoremove files after 120 min
+# cat /etc/crontab
+# */5 * * * * <username> /usr/bin/docker exec -it -d <container_name> bash -c "bash /etc/Scripts/2hourcleanup.sh" > /dev/null
 time=$(date '+%d-%m-%Y_%H:%M:%S')
 find /var/www/YT/tmp/ -type f -cmin +120 -delete
 find /var/www/YT/download/ -type f -cmin +120 -delete
