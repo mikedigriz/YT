153972130e0992ad7719ee881e5678a182790c95
mikedigriz <mik.gryzunov@gmail.com>
2026-03-28 16:28:13 +0300

    fix potential vuln
    update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -e
 
-# Сохраняем версию yt-dlp
 yt-dlp --version > /var/www/YT/yt_dlp_version
 
-# Запускаем настройку логгера
 ./logger.sh
 
-# Запускаем сервисы
-service php8.4-fpm start
-service nginx start
-
-exec tail -F /var/log/yt_dlp.log
+php-fpm8.4 -F &
+exec nginx -g "daemon off;"
